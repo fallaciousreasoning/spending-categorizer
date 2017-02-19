e8763ee75ae89e8259de21bebb45e2dfde1b5c1b
Jay Harris <jay.harris@outlook.co.nz>
2017-02-19 14:17:27 +1300

    Improves method for adding categorizers and runs them over the file

--- src/analyser.rs
+++ src/analyser.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone)]
 pub enum Category {
     Supermarket,
     Movies,
@@ -21,6 +22,25 @@ pub fn categorize(description : String, categorizers : Vec<Categorizer>) -> Vec<
      return result;
 }
 
-pub fn initDefaultCategorizers(to : Vec<Categorizer>) {
-    to.push(Box(|s| -> Some(Category::Supermarket)));
+pub fn initDefaultCategorizers() -> Vec<Categorizer> {
+    let mut result : Vec<Categorizer> = vec![];
+
+    result.push(containsMapping("NEW WORLD".to_string(), Category::Supermarket));
+    result.push(containsMapping("COUNTDOWN".to_string(), Category::Supermarket));
+    result.push(containsMapping("PAKNSAVE".to_string(), Category::Supermarket));
+
+    result.push(containsMapping("KINDLE".to_string(), Category::Books));
+    result.push(containsMapping("AMAZON".to_string(), Category::Amazon));
+
+    return result;
+}
+
+pub fn containsMapping(c : String, category : Category) -> Categorizer {
+    let func : Box<Fn(String) -> Option<Category>> = Box::new(move |desc| {
+        if desc.contains(&c.clone()[..]) {
+            return Some(category.clone());
+        }
+        return None;
+    });
+    return func;
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,18 @@ mod analyser;
 
 fn main() {
     let mut rdr = csv::Reader::from_file("/Users/jayharris/money/data/statement.csv").unwrap();
+    
     for record in rdr.decode() {
         let row : statement::StatementRow = record.unwrap();
-        println!("{} for {} leaving {}", row.description, row.total_amount, row.balance);
+        let description = row.description.to_string();
+        let categorizers = analyser::initDefaultCategorizers();
+        let result = analyser::categorize(row.description, categorizers);
+        let len = result.len();
+
+        for category in result {
+            print!("{} -> {:?}, ", description, category);
+        }
+        
+        if len > 0 { println!(); }
     }
 }
